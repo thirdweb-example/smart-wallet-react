18aa44d80fedbb6e69bf385962961370174fdd27
Atharva Deosthale <atharva.deosthale17@gmail.com>
2023-05-04 17:12:25 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
+import {
+  ConnectWallet,
+  Web3Button,
+  useAddress,
+  useContract,
+  useNFT,
+  useOwnedNFTs,
+} from "@thirdweb-dev/react";
 import "./styles/Home.css";
+import { editionDropAddress, editionDropTokenId } from "../const/yourDetails";
 
 export default function Home() {
+  const address = useAddress();
+
+  const { contract: editionDropContract } = useContract(
+    editionDropAddress,
+    "edition-drop"
+  );
+  const { data: nft, isLoading: isNftLoading } = useNFT(
+    editionDropContract,
+    editionDropTokenId
+  );
+  const { data: ownedNfts, refetch: refetchOwnedNfts } = useOwnedNFTs(
+    editionDropContract,
+    address
+  );
+
   return (
     <div className="container">
       <main className="main">
@@ -10,42 +33,50 @@ export default function Home() {
         </h1>
 
         <p className="description">
-          Get started by configuring your desired network in{" "}
-          <code className="code">src/main.jsx</code>, then modify the{" "}
-          <code className="code">src/App.jsx</code> file!
+          Claim your test access pass by creating an account!
         </p>
 
         <div className="connect">
-          <ConnectWallet dropdownPosition={{
-            align: 'center',
-            side: 'bottom'
-          }} />
+          <ConnectWallet
+            dropdownPosition={{
+              align: "center",
+              side: "bottom",
+            }}
+            btnTitle="Login"
+          />
         </div>
 
-        <div className="grid">
-          <a href="https://portal.thirdweb.com/" className="card">
-            <h2>Portal &rarr;</h2>
-            <p>
-              Guides, references and resources that will help you build with
-              thirdweb.
-            </p>
-          </a>
-
-          <a href="https://thirdweb.com/dashboard" className="card">
-            <h2>Dashboard &rarr;</h2>
-            <p>
-              Deploy, configure and manage your smart contracts from the
-              dashboard.
-            </p>
-          </a>
-
-          <a href="https://portal.thirdweb.com/templates" className="card">
-            <h2>Templates &rarr;</h2>
-            <p>
-              Discover and clone template projects showcasing thirdweb features.
-            </p>
-          </a>
-        </div>
+        {isNftLoading ? (
+          "Loading..."
+        ) : (
+          <div className="card">
+            <img
+              className="nftImage"
+              src={nft.metadata.image}
+              alt={nft.metadata.description}
+            />
+            {address ? (
+              <>
+                <p>You own {ownedNfts?.[0]?.quantityOwned || "0"}</p>
+                <Web3Button
+                  contractAddress={editionDropAddress}
+                  action={(contract) =>
+                    contract.erc1155.claim(editionDropTokenId, 1)
+                  }
+                  onSuccess={async () => {
+                    await refetchOwnedNfts();
+                    alert("Claim successful!");
+                  }}
+                  style={{ width: "100%", marginTop: "10px" }}
+                >
+                  Claim!
+                </Web3Button>
+              </>
+            ) : (
+              <p>Login to claim!</p>
+            )}
+          </div>
+        )}
       </main>
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,30 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import { ThirdwebProvider } from "@thirdweb-dev/react";
+import {
+  ThirdwebProvider,
+  localWallet,
+  metamaskWallet,
+  smartWallet,
+} from "@thirdweb-dev/react";
 import "./styles/globals.css";
-
-// This is the chain your dApp will work on.
-// Change this to the chain your app is built for.
-// You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
+import { TWApiKey, TWFactoryAddress, activeChain } from "../const/yourDetails";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
-    <ThirdwebProvider activeChain={activeChain}>
+    <ThirdwebProvider
+      activeChain={activeChain}
+      supportedWallets={[
+        smartWallet({
+          factoryAddress: TWFactoryAddress,
+          thirdwebApiKey: TWApiKey,
+          gasless: true,
+          personalWallets: [metamaskWallet(), localWallet({ persist: true })],
+        }),
+      ]}
+    >
       <App />
     </ThirdwebProvider>
   </React.StrictMode>
