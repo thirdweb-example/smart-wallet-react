1fe033ff61bac1d87f62441de8ad7b031f462d22
Joaquim Verges <joaquim.verges@gmail.com>
2023-09-01 12:27:20 -0700

    Initial commit from npx thirdweb create

---
+++ src/main.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+import "./styles/globals.css";
+
+// This is the chain your dApp will work on.
+// Change this to the chain your app is built for.
+// You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
+const activeChain = "ethereum";
+
+const container = document.getElementById("root");
+const root = createRoot(container!);
+root.render(
+  <React.StrictMode>
+    <ThirdwebProvider
+      clientId={import.meta.env.VITE_TEMPLATE_CLIENT_ID}
+      activeChain={activeChain}
+    >
+      <App />
+    </ThirdwebProvider>
+  </React.StrictMode>
+);
